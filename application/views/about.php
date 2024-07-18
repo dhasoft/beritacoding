a65975e6a60671796fd224a769937e0ba55fdf94
Dhana <dhacell26@gmail.com>
2024-07-18 19:27:24 +0700

    ubah Judul & paragraf file about.php

--- application/views/about.php
+++ application/views/about.php
@@ -8,7 +8,10 @@
 <body>
     <?php $this->load->view('_partials/navbar.php'); ?>
 
-    <h1>About Page</h1>
+    <h1>Tentang Kami</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus deserunt commodi error consequatur, asperiores
+        recusandae illum distinctio nulla totam officia obcaecati pariatur incidunt earum eius voluptatem quas nihil
+        voluptate aspernatur.</p>
 
     <?php $this->load->view('_partials/footer.php'); ?>
 </body>
